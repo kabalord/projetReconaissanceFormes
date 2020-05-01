d8d2e9a5725b1aec1d283ad0ff56921d7e8c7dc2
kabalord <wroaserrano@gmail.com>
2020-05-01 20:03:27 +0200

    LDP Kirsh Algorithm implémentation

--- script.py
+++ script.py
@@ -1,14 +1,6 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Apr 30 21:33:35 2020
-
-@author: walterroaserrano
-"""
-
-## Call the opencv library and numpy
-import cv2
 import numpy as np
+import cv2
+
 
 ## RGB image
 imgColor = cv2.imread('/Users/walterroaserrano/Desktop/UniversiteChampagneArdenne/reconnaissanceFormes/projetReconaissanceFormes/example1.jpg')
@@ -30,4 +22,58 @@ print (img.shape)
 img.size
 
 ## Write an image 
-cv2.imwrite('gray image.png', img)
+cv2.imwrite('gray_image.png', img)
+
+def kirsch_filter(gray):
+    if gray.ndim > 2:
+        raise Exception("illegal argument: input must be a single channel image (gray)")
+    kernelG1 = np.array([[ 5,  5,  5],
+                         [-3,  0, -3],
+                         [-3, -3, -3]], dtype=np.float32)
+    kernelG2 = np.array([[ 5,  5, -3],
+                         [ 5,  0, -3],
+                         [-3, -3, -3]], dtype=np.float32)
+    kernelG3 = np.array([[ 5, -3, -3],
+                         [ 5,  0, -3],
+                         [ 5, -3, -3]], dtype=np.float32)
+    kernelG4 = np.array([[-3, -3, -3],
+                         [ 5,  0, -3],
+                         [ 5,  5, -3]], dtype=np.float32)
+    kernelG5 = np.array([[-3, -3, -3],
+                         [-3,  0, -3],
+                         [ 5,  5,  5]], dtype=np.float32)
+    kernelG6 = np.array([[-3, -3, -3],
+                         [-3,  0,  5],
+                         [-3,  5,  5]], dtype=np.float32)
+    kernelG7 = np.array([[-3, -3,  5],
+                         [-3,  0,  5],
+                         [-3, -3,  5]], dtype=np.float32)
+    kernelG8 = np.array([[-3,  5,  5],
+                         [-3,  0,  5],
+                         [-3, -3, -3]], dtype=np.float32)
+
+    g1 = cv2.normalize(cv2.filter2D(gray, cv2.CV_32F, kernelG1), None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8UC1)
+    g2 = cv2.normalize(cv2.filter2D(gray, cv2.CV_32F, kernelG2), None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8UC1)
+    g3 = cv2.normalize(cv2.filter2D(gray, cv2.CV_32F, kernelG3), None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8UC1)
+    g4 = cv2.normalize(cv2.filter2D(gray, cv2.CV_32F, kernelG4), None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8UC1)
+    g5 = cv2.normalize(cv2.filter2D(gray, cv2.CV_32F, kernelG5), None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8UC1)
+    g6 = cv2.normalize(cv2.filter2D(gray, cv2.CV_32F, kernelG6), None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8UC1)
+    g7 = cv2.normalize(cv2.filter2D(gray, cv2.CV_32F, kernelG7), None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8UC1)
+    g8 = cv2.normalize(cv2.filter2D(gray, cv2.CV_32F, kernelG8), None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8UC1)
+    magn = cv2.max(
+        g1, cv2.max(
+            g2, cv2.max(
+                g3, cv2.max(
+                    g4, cv2.max(
+                        g5, cv2.max(
+                            g6, cv2.max(
+                                g7, g8
+                            )
+                        )
+                    )
+                )
+            )
+        )
+    )
+    return magn
+
